b2dd4e678b6b564c2d6528b19c19a5fd4a113b7f
Kamil Rajtar <Kamil@rajtar.pl>
2016-04-26 16:56:12 +0200

    After cleanup

--- app/src/main/java/com/example/kamil/fotosforwebsite/Fotos.java
+++ app/src/main/java/com/example/kamil/fotosforwebsite/Fotos.java
@@ -1,6 +1,7 @@
 package com.example.kamil.fotosforwebsite;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -16,56 +17,29 @@ import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Comparator;
-//import android.support.v7.app.ActionBarActivity;
-//import android.view.Menu;
-//import android.view.MenuItem;
 
 
 public class Fotos extends AppCompatActivity
 {
+	private String photoDirectory="";
+	private File[] photos;
+	private int current;
 
-	public String photoDirectory="";
-
-	public File photos[];
-	public int current=0;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_fotos);
-	}
-
-	public String getRealPathFromURI(Uri contentUri)
-	{
-		Log.w("getRealPathFromURI","State1");
-		String[] project={MediaStore.Images.Media.DATA};
-		Log.w("getRealPathFromURI","State2");
-		Log.w("contentUri",contentUri.toString());
-		//noinspection deprecation
-		android.database.Cursor cursor=managedQuery(contentUri,project,     // Which columns to return
-				null,     // WHERE clause; which rows to return (all rows)
-				null,     // WHERE clause selection arguments (none)
-				null);     // Order-by clause (ascending by name)
-		int column_index=cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-		Log.w("getRealPathFromURI","State3");
-		cursor.moveToFirst();
-		Log.w("getRealPathFromURI","State4");
-		return cursor.getString(column_index);
-	}
-	public Bitmap getRotatedBitmap(File imgFile)
+	private static Bitmap getRotatedBitmap(final File imgFile)
 	{
 		Bitmap myBitmap=BitmapFactory.decodeFile(imgFile.getAbsolutePath());
 		try
 		{
-			ExifInterface exif=new ExifInterface(imgFile.getAbsolutePath());
-			int orientation=exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,1);
-			Matrix matrix=new Matrix();
-			Log.w("ROTATION","ROTATION START");
+			final ExifInterface exif=new ExifInterface(imgFile.getAbsolutePath());
+			final int orientation=exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,1);
+			final Matrix matrix=new Matrix();
 			switch(orientation)
 			{
 				case 3:
@@ -80,186 +54,135 @@ public class Fotos extends AppCompatActivity
 					matrix.postRotate(90);
 					myBitmap=Bitmap.createBitmap(myBitmap,0,0,myBitmap.getWidth(),myBitmap.getHeight(),matrix,true);
 					break;
-
 			}
-
-		}
-		catch(Exception e)
+		}catch(final IOException e)
 		{
 			e.printStackTrace();
 		}
 		return myBitmap;
 	}
-	@Override
-	protected void onActivityResult(int requestCode,int resultCode,Intent data)
-	{
 
-		Log.w("mam PATH","catch");
-		if(requestCode==0xDEAD) // Check which request we're responding to
+	@Override
+	protected void onCreate(final Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fotos);
+	}
+
+	private String getRealPathFromURI(final Uri contentUri)
+	{
+		final String[] project={MediaStore.MediaColumns.DATA};
+		try(Cursor cursor=getApplicationContext().getContentResolver()
+				.query(contentUri,project,     // Which columns to return
+				null,     // WHERE clause; which rows to return (all rows)
+				null,     // WHERE clause selection arguments (none)
+						null))
 		{
-			if(resultCode==RESULT_OK) // Make sure the request was successful
+			assert cursor!=null;
+			final int column_index=cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+			cursor.moveToFirst();
+			return cursor.getString(column_index);
+		}
+	}
+
+	@Override
+	protected void onActivityResult(final int requestCode,final int resultCode,final Intent data)
+	{
+		if(requestCode==0xDEAD)
+		{
+			if(resultCode==RESULT_OK)
 			{
 				current=0;
-				// The user picked a photo.
-				// The Intent's data Uri identifies which contact was selected.
-				Log.w("mam PATH","catch2");
-				Uri uri=data.getData();
-				Log.w("mam PATH","catch3");
-				String PATH=getRealPathFromURI(uri);
-				Log.w("mam PATH","catch4");
-				int index=PATH.lastIndexOf("/");
+				final Uri uri=data.getData();
+				final String PATH=getRealPathFromURI(uri);
+				final int index=PATH.lastIndexOf('/');
 				photoDirectory=PATH.substring(0,index);
-				Log.w("mam PATH",photoDirectory);
-				try
+				photos=new File(photoDirectory).listFiles(new FilenameFilter()
 				{
-					photos=new File(photoDirectory).listFiles(new FilenameFilter()
+					@Override
+					public boolean accept(final File dir,final String name)
 					{
-						public boolean accept(File dir,String name)
-						{
-							return name.toLowerCase().endsWith(".jpg");
-						}
-					});
-					Arrays.sort(photos,new Comparator<File>(){
-						@Override
-						public int compare(File lhs,File rhs)
-						{
-							return lhs.getName().compareTo(rhs.getName());
-						}
-					});
-				}catch(Exception e)
-				{
-					e.printStackTrace();
-				}
-				//    Log.w("mam PATH",fileName);
-				//try
-				//{
-				//    TextView t=new TextView(this);
-				//    t=(TextView)findViewById(R.id.textView2);
-				//    t.setText(PATH);
-				//}catch (Exception e) {
-				//    Log.e("TextView Exception",e.toString());
-				//}
-				//ImageView i=new ImageView(R.id.imageView);
-				//i.setImageResource();
-				try
-				{
-					File imgFile=new File(String.valueOf(photos[current]));
-					Log.d("ShowedFile",photos[current].toString());
-					if(imgFile.exists())
-					{
-						Bitmap myBitmap=getRotatedBitmap(imgFile);
-								/*BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-						ExifInterface exif = new ExifInterface(imgFile.getAbsolutePath());
-						int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
-						Matrix matrix = new Matrix();
-						Log.w("ROTATION","ROTATION START");
-						switch(orientation)
-						{
-							case 3:
-								matrix.postRotate(180);
-								myBitmap= Bitmap.createBitmap(myBitmap, 0, 0, myBitmap.getWidth(), myBitmap.getHeight(), matrix, true);
-						        break;
-							case 6:
-								matrix.postRotate(90);
-								myBitmap= Bitmap.createBitmap(myBitmap, 0, 0, myBitmap.getWidth(), myBitmap.getHeight(), matrix, true);
-								break;
-							case 8:
-								matrix.postRotate(90);
-								myBitmap= Bitmap.createBitmap(myBitmap, 0, 0, myBitmap.getWidth(), myBitmap.getHeight(), matrix, true);
-								break;
-
-						}*/
-						ImageView myImage=(ImageView)findViewById(R.id.imageView);
-						int nh=(int)(myBitmap.getHeight()*(512.0/myBitmap.getWidth()));
-						Bitmap scaled=Bitmap.createScaledBitmap(myBitmap,512,nh,true);
-						myImage.setImageBitmap(scaled);
+						return name.toLowerCase().endsWith(".jpg");
 					}
-				}catch(Exception e)
+				});
+				Arrays.sort(photos,new Comparator<File>()
 				{
-					Log.e("Return File Exception",e.toString());
+					@Override
+					public int compare(final File lhs,final File rhs)
+					{
+						return lhs.getName().compareTo(rhs.getName());
+					}
+				});
+				final File imgFile=new File(String.valueOf(photos[current]));
+				Log.d("ShowedFile",photos[current].toString());
+				if(imgFile.exists())
+				{
+					final Bitmap myBitmap=getRotatedBitmap(imgFile);
+					final ImageView myImage=(ImageView)findViewById(R.id.imageView);
+					final int nh=(int)(myBitmap.getHeight()*(512.0/myBitmap.getWidth()));
+					final Bitmap scaled=Bitmap.createScaledBitmap(myBitmap,512,nh,true);
+					myImage.setImageBitmap(scaled);
 				}
 			}
 		}
 	}
 
-	public void nextPhoto(View view)
+	public void nextPhoto(final View view)
 	{
-		if(photoDirectory.equals(""))
-		{
+		if(photoDirectory.isEmpty())
 			return;
-		}
-		//getting description
-		try
+		final String PATH=photos[current].toString();
+		final int index=PATH.lastIndexOf('.');
+		try(PrintWriter writer=new PrintWriter(PATH.substring(0,index)+".txt","UTF-8"))
 		{
-			TextView textField=(TextView)findViewById(R.id.editText);
-			String description=textField.getText().toString();
-			Log.d("Description",description);
+			final TextView textField=(TextView)findViewById(R.id.editText);
+			writer.println(textField.getText());
 			textField.setText("");
-			//creating text file
-			File describedPhoto=photos[current];
-			String PATH=describedPhoto.toString();
-			int index=PATH.lastIndexOf(".");
-			PrintWriter writer=new PrintWriter(PATH.substring(0,index)+".txt","UTF-8");
-			writer.println(description);
-			writer.close();
-			Log.d("DescriptionPATH",PATH.substring(0,index)+".txt");
-
-		}catch(Exception e)
+		}catch(FileNotFoundException|UnsupportedEncodingException e)
 		{
 			e.printStackTrace();
 		}
-		if(view==findViewById(R.id.button2))
+		if(view.equals(findViewById(R.id.button2)))
 		{
 			current++;
 			current%=photos.length;
 		}
-		else if(view==findViewById(R.id.button3))
+		else if(view.equals(findViewById(R.id.button3)))
 		{
 			current--;
 			current+=photos.length;
 			current%=photos.length;
 		}
-		try
+		final File imgFile=new File(String.valueOf(photos[current]));
+		if(imgFile.exists())
 		{
-			File imgFile=new File(String.valueOf(photos[current]));
-			Log.d("ShowedFile",photos[current].toString());
-			if(imgFile.exists())
+			final Bitmap myBitmap=getRotatedBitmap(imgFile);
+			final int nh=(int)(myBitmap.getHeight()*(512.0/myBitmap.getWidth()));
+			final Bitmap scaled=Bitmap.createScaledBitmap(myBitmap,512,nh,true);
+			((ImageView)findViewById(R.id.imageView)).setImageBitmap(scaled);
+		}
+		final String PATH2=photos[current].toString();
+		final int index2=PATH2.lastIndexOf('.');
+		final File descriptionFile=new File(PATH2.substring(0,index2)+".txt");
+		if(descriptionFile.exists())
+		{
+			try(FileInputStream fis=new FileInputStream(descriptionFile))
 			{
-				Bitmap myBitmap=getRotatedBitmap(imgFile);//BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-				ImageView myImage=(ImageView)findViewById(R.id.imageView);
-				int nh=(int)(myBitmap.getHeight()*(512.0/myBitmap.getWidth()));
-				Bitmap scaled=Bitmap.createScaledBitmap(myBitmap,512,nh,true);
-				myImage.setImageBitmap(scaled);
-			}
-			File describedPhoto=photos[current];
-			String PATH=describedPhoto.toString();
-			int index=PATH.lastIndexOf(".");
-			File descriptionFile=new File(PATH.substring(0,index)+".txt");
-			if(descriptionFile.exists())
-			{
-				FileInputStream fis=new FileInputStream(descriptionFile);
-				byte[] data=new byte[(int)descriptionFile.length()];
-				//noinspection ResultOfMethodCallIgnored
+				final byte[] data=new byte[(int)descriptionFile.length()];
 				fis.read(data);
-				fis.close();
-				String description=new String(data,"UTF-8");
-				TextView textField=(TextView)findViewById(R.id.editText);
-				textField.setText(description);
+				((TextView)findViewById(R.id.editText)).setText(new String(data,"UTF-8"));
+			}catch(final IOException e)
+			{
+				e.printStackTrace();
 			}
-		}catch(Exception e)
-		{
-			Log.e("Return File Exception",e.toString());
 		}
 	}
 
-	public void selectDirectory(View view)
+	public void selectDirectory(@SuppressWarnings("UnusedParameters") final View ignored)
 	{
-		Intent fileIntent=new Intent(Intent.ACTION_GET_CONTENT);
-		fileIntent.setType("file/*"); // intent type to filter application based on your requirement
-		Intent intent=new Intent();
+		final Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
 		intent.setType("image/*");
-		intent.setAction(Intent.ACTION_GET_CONTENT);
-		int REQUEST_CODE=0xDEAD;
+		final int REQUEST_CODE=0xDEAD;
 		startActivityForResult(Intent.createChooser(intent,"Select Picture"),REQUEST_CODE);
 	}
 }
